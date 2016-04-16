6eb8d6483dd5bc194e2ab56ef87e69a64b3a5ca5
Kien T <kienpc81089@gmail.com>
2016-04-16 21:31:28 +0700

    Section features phase 1

---
+++ section-parts/section-about.php
@@ -0,0 +1,11 @@
+<section id="" class="section-about section-padding onepage-section">
+    <div class="container">
+        <div class="container">
+            <div class="section-title-area">
+                <h2 class="section-title">Section Title</h2>
+                <h5 class="section-subtitle">Section Subtitle</h5>
+                <div class="section-desc">Section Description</div>
+            </div>
+        </div>
+    </div>
+</section>
