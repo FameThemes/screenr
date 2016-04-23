17c21f85e1fdb657fe71d62b87fc41a07af4516e
shrimp2t <shrimp2t@gmail.com>
2016-04-23 13:09:19 +0700

    Restructure Assets fixed #9

--- functions.php
+++ functions.php
@@ -129,20 +129,13 @@ add_action( 'widgets_init', 'screenr_widgets_init' );
  */
 function screenr_scripts() {
 	wp_enqueue_style( 'screenr-fonts', screenr_fonts_url(), array(), null );
-	wp_enqueue_style( 'font-awesome', get_template_directory_uri() .'/assets/font-awesome/css/font-awesome.min.css', false, '4.0.0' );
+	wp_enqueue_style( 'font-awesome', get_template_directory_uri() .'/assets/css/font-awesome.min.css', false, '4.0.0' );
 	wp_enqueue_style( 'bootstrap', get_template_directory_uri() .'/assets/css/bootstrap.min.css', false, '4.0.0' );
 	wp_enqueue_style( 'screenr-style', get_stylesheet_uri() );
 
-
-	wp_enqueue_script( 'screenr-plugin', get_template_directory_uri() . '/assets/js/plugin.js', array(), '4.0.0', true );
-	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array(), '4.0.0', true );
-
 	wp_enqueue_script( 'jquery' );
-
-
-	wp_enqueue_style( 'swiper', get_template_directory_uri() .'/assets/swiper/css/swiper.min.css', false, '4.0.0' );
-	wp_enqueue_script( 'swiper', get_template_directory_uri() . '/assets/swiper/js/swiper.jquery.min.js', array( 'jquery' ), '4.0.0', true );
-
+	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array(), '4.0.0', true );
+	wp_enqueue_script( 'screenr-plugin', get_template_directory_uri() . '/assets/js/plugin.js', array( 'jquery' ), '4.0.0', true );
 	wp_enqueue_script( 'screenr-theme', get_template_directory_uri() . '/assets/js/theme.js', array( 'jquery' ), '20120206', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
