b10a54a28b162f36e54809985c563ad6d81d2142
shrimp2t <shrimp2t@gmail.com>
2016-06-07 17:57:04 +0700

    Add footer widget conditonal #45

--- footer.php
+++ footer.php
@@ -13,42 +13,53 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<?php
 		$footer_columns = absint( get_theme_mod( 'footer_layout' , 4 ) );
-		if ( $footer_columns > 0 ) { ?>
-			<div class="footer-widgets section-padding">
+
+		$max_cols = 12;
+		$layouts = 12;
+		if ( $footer_columns > 1 ){
+			$default = "12";
+			switch ( $footer_columns ) {
+				case 4:
+					$default = '3+3+3+3';
+					break;
+				case 3:
+					$default = '4+4+4';
+					break;
+				case 2:
+					$default = '6+6';
+					break;
+			}
+			$layouts = get_theme_mod( 'footer_custom_'.$footer_columns.'_columns', $default );
+		}
+
+		$layouts = explode( '+', $layouts );
+		foreach ( $layouts as $k => $v ) {
+			$v = absint( trim( $v ) );
+			$v =  $v >= $max_cols ? $max_cols : $v;
+			$layouts[ $k ] = $v;
+		}
+
+		$have_widgets = false;
+
+		for ( $count = 0; $count < $footer_columns; $count++ ) {
+			$id = 'footer-' . ( $count + 1 );
+			if ( is_active_sidebar( $id ) ) {
+				$have_widgets = true;
+			}
+		}
+
+		if ( $footer_columns > 0 && $have_widgets ) { ?>
+			<div class="footer-widgets section-padding ">
 				<div class="container">
 					<div class="row">
 						<?php
-						$max_cols = 12;
-						$layouts = 12;
-						if ( $footer_columns > 1 ){
-							$default = "12";
-							switch ( $footer_columns ) {
-								case 4:
-									$default = '3+3+3+3';
-									break;
-								case 3:
-									$default = '4+4+4';
-									break;
-								case 2:
-									$default = '6+6';
-									break;
-							}
-							$layouts = get_theme_mod( 'footer_custom_'.$footer_columns.'_columns', $default );
-						}
-
-						$layouts = explode( '+', $layouts );
-						foreach ( $layouts as $k => $v ) {
-							$v = absint( trim( $v ) );
-							$v =  $v >= $max_cols ? $max_cols : $v;
-							$layouts[ $k ] = $v;
-						}
-
 						for ( $count = 0; $count < $footer_columns; $count++ ) {
 							$col = isset( $layouts[ $count ] ) ? $layouts[ $count ] : '';
+							$id = 'footer-' . ( $count + 1 );
 							if ( $col ) {
 								?>
 								<div id="footer-<?php echo esc_attr( $count + 1 ) ?>" class="col-md-<?php echo esc_attr( $col ); ?> col-sm-12 footer-column widget-area sidebar" role="complementary">
-									<?php dynamic_sidebar('footer-' . ( $count + 1 ) ); ?>
+									<?php dynamic_sidebar( $id ); ?>
 								</div>
 								<?php
 							}
