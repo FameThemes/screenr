73e9292e6cf538c02feeebb7e4163cc121cd1a02
TruongSa <shrimp2t@gmail.com>
2016-07-07 08:45:18 +0700

    Fixed #65

--- index.php
+++ index.php
@@ -19,16 +19,11 @@ get_header(); ?>
 			<div id="primary" class="content-area">
 				<main id="main" class="site-main" role="main">
 
-				<?php
-				if ( have_posts() ) :
-
-					if ( is_home() && ! is_front_page() ) : ?>
-						<header>
-							<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-						</header>
-
+				<?php if ( have_posts() ) : ?>
+                    <header>
+                        <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+                    </header>
 					<?php
-					endif;
 
 					/* Start the Loop */
 					while ( have_posts() ) : the_post();
