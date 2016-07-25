543ba0072b44a1d0bc3923820ce7efb66a863d3d
shrimp2t <shrimp2t@gmail.com>
2016-07-25 08:17:39 +0700

    Add original version of js and css lib, #84

--- functions.php
+++ functions.php
@@ -166,7 +166,7 @@ function screenr_scripts() {
 	wp_enqueue_style( 'bootstrap', get_template_directory_uri() .'/assets/css/bootstrap.min.css', false, '4.0.0' );
 	wp_enqueue_style( 'screenr-style', get_stylesheet_uri() );
 
-    wp_enqueue_script( 'screenr-plugin', get_template_directory_uri() . '/assets/js/plugin.js', array( 'jquery' ), '4.0.0', true );
+    wp_enqueue_script( 'screenr-plugin', get_template_directory_uri() . '/assets/js/plugins.js', array( 'jquery' ), '4.0.0', true );
 	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array(), '4.0.0', true );
 	wp_enqueue_script( 'screenr-theme', get_template_directory_uri() . '/assets/js/theme.js', array( 'jquery' ), '20120206', true );
 
