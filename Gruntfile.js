a5cdab8f3013af7158144d669291c76860aa3549
TruongTuyen <hieudieuluong@gmail.com>
2021-09-21 10:38:18 +0700

    Update gruntjs

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,6 +68,7 @@ module.exports = function( grunt ) {
                     '!phpunit.xml.dist',
                     '!*.sh',
                     '!*.map',
+                    '!*.js.map',
                     '!Gruntfile.js',
                     '!package.json',
                     '!.gitignore',
